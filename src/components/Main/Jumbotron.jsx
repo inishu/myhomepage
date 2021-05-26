4177306fbeb2058afeca701d312be77f3fca1012
Nishant Srivastava <codeofnishant@gmail.com>
2021-05-26 16:23:28 +0530

    My first commit reactapp website

--- src/components/Main/Jumbotron.jsx
+++ src/components/Main/Jumbotron.jsx
@@ -5,7 +5,7 @@ import "./Jumbotron.css";
 const Jumbotrn = () => {
     return (
         <div className="jumbotron text-center" >
-                <img src="/image/NISHANT.png" alt="" />
+                <img src="image/NISHANT.png" alt="" />
                 <h1>Nishant Srivastava</h1>
                  <p>Electrical Engineer | React Developer | Programmer | IOT</p>
         </div>
