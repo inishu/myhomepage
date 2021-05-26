e0e4752be5cd0a65ad90ccb506ecad3d0dbea889
Nishant Srivastava <codeofnishant@gmail.com>
2021-05-26 15:45:33 +0530

    Updates

---
+++ precache-manifest.2e3d60b27d2e5e42b5b9bfd277654cc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148027c975fcfe5a25add777188bc5a1",
+    "url": "/myhomepage/index.html"
+  },
+  {
+    "revision": "90cd16acca4654f57db7",
+    "url": "/myhomepage/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "620b2cb40944533e0bc9",
+    "url": "/myhomepage/static/css/main.e151bc34.chunk.css"
+  },
+  {
+    "revision": "90cd16acca4654f57db7",
+    "url": "/myhomepage/static/js/2.69360c92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myhomepage/static/js/2.69360c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620b2cb40944533e0bc9",
+    "url": "/myhomepage/static/js/main.7fa87ad7.chunk.js"
+  },
+  {
+    "revision": "0b6d07b4964fcab44193",
+    "url": "/myhomepage/static/js/runtime-main.54099f02.js"
+  }
+]);
